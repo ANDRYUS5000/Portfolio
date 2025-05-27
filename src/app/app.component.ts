7c84db84b5745cefd251be2277761a9b8cb5fb5c
Andres Mora <andresmora555@hotmail.com>
2025-05-27 18:21:01 -0500

    angular 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { Menu } from 'primeng/menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'port';
+
+  items: MenuItem[] | undefined;
+
+    ngOnInit() {
+        this.items = [
+            { label: 'New', icon: 'pi pi-plus' },
+            { label: 'Search', icon: 'pi pi-search' }
+        ];
+    }
 }
--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -3,12 +3,14 @@ import { provideServerRendering } from '@angular/platform-server';
 import { provideServerRouting } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
+// This file is used to configure the server-side rendering for the Angular application.
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
     provideServerRouting(serverRoutes)
-  ]
+  ],
+
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
